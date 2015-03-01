6f329a7569a041cb5edbf6366aa0b0339a91ee27
Jennifer Jiyue Wang <jiyue@stanford.edu>
2015-02-28 17:09:47 -0800

    update get_data

---
+++ dataset/get_dataset.sh
@@ -0,0 +1,6 @@
+# Get chars74k EnglishImg
+wget http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/EnglishImg.tgz
+wget http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/EnglishFnt.tgz
+#wget http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/EnglishHnd.tgz
+tar -xzvf EnglishImg.tgz EnglishFnt.tgz
+rm EnglishImg.tgz EnglishFnt.tgz 

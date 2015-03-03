26bdd72080ec2f6c34cc932cbd6b20930d70dfff
Jiyue Wang <jiyue@stanford.edu>
2015-03-02 23:09:17 -0500

    nothing to commit

--- code_for_caffe/models/char74_train.sh
+++ code_for_caffe/models/char74_train.sh
@@ -1,1 +1,1 @@
-~/caffe/build/tools/caffe train -solver solver.prototxt -weights ../caffe/examples/mnist/lenet_iter_10000.caffemodel -gpu 0
+GLOG_logtostderr=1 ~/caffe/build/tools/caffe train -solver char74_solver.prototxt -weights lenet_iter_10000.caffemodel -gpu 0

54d7d31ff6fb350ab4eafa049c6661ce021e78db
Jennifer Jiyue Wang <jiyue@stanford.edu>
2015-03-02 19:22:19 -0800

    update

--- code_for_caffe/create_img_good.sh
+++ code_for_caffe/create_img_good.sh
@@ -7,7 +7,7 @@ python ./generate_data_labels.py $ROOT_DIR $Label_DIR train_good.txt val_good.tx
 
 NEW_DIR=./dataset
 DATA=./dataset
-TOOLS=../caffe/build/tools
+TOOLS=~/caffe/build/tools
 
 TRAIN_DATA_ROOT=../data/English/Img/GoodImg/Bmp/
 VAL_DATA_ROOT=../data/English/Img/GoodImg/Bmp/

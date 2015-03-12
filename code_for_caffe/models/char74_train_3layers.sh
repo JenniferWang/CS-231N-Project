dfe60d779f3fee93fdbd9eaf75c3c9e6ac9ba865
Jiyue Wang <jiyue@stanford.edu>
2015-03-12 19:21:52 -0400

    delete char74

--- code_for_caffe/models/char74_train_3layers.sh
+++
@@ -1,1 +0,0 @@
-GLOG_logtostderr=1 ~/caffe/build/tools/caffe train -solver char74_solver_3layers.prototxt -weights lenet_iter_10000.caffemodel -gpu 0
--- code_for_caffe/models/char74_train_3layers_good_image.sh
+++
@@ -1,1 +0,0 @@
-GLOG_logtostderr=1 ~/caffe/build/tools/caffe train -solver char74_solver_3layers_good_image.prototxt -weights 3layers_new_iter_50000.caffemodel -gpu 0
